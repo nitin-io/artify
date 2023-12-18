99c5219f26ad19b0f8780d79150049a04e793218
Nitin Chaudhary <nitin.io@outlook.com>
2023-12-18 09:01:56 +0530

    Fix Top-level await problem

--- src/js/index.js
+++ src/js/index.js
@@ -3,11 +3,9 @@ import { renderGallery } from "./view/gallery";
 
 const popUpModal = document.querySelector("dialog");
 const modalCloseBtn = document.querySelector("[data-close-modal]");
-const logoBtn = document.getElementById("logo-btn");
 
 // Rendering Gallery
-renderGallery(await getImages());
-
+(async () => renderGallery(await getImages()))();
 // Event Listener to close modal
 modalCloseBtn.addEventListener("click", () => {
   popUpModal.close();
